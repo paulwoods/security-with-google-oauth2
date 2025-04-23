c9adbf4b415827634bf5e2e62155a636f2772736
mrpaulwoods <mr.paul.woods@gmail.com>
2025-04-23 09:52:33 -0500

    feat: initial commit
    - Set up Gradle build configuration
    - Configure project structure and dependencies
    - Add .gitignore and .gitattributes
    - Include application.properties with app name

---
+++ src/main/java/com/mrpaulwoods/security/google/demo/securitywithgoogleoauth2/SecurityWithGoogleOauth2Application.java
@@ -0,0 +1,13 @@
+package com.mrpaulwoods.security.google.demo.securitywithgoogleoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWithGoogleOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityWithGoogleOauth2Application.class, args);
+    }
+
+}
